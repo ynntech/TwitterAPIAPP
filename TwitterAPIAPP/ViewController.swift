1b369945916eb1cf29016a77db211b0e243587d1
Yushi Nakaya <math271k@gmail.com>
2019-08-18 21:16:29 +0900

    Initial Commit

---
+++ TwitterAPIAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterAPIAPP
+//
+//  Created by Yushi Nakaya on 2019/08/18.
+//  Copyright © 2019 Yushi Nakaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
